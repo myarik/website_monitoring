e0ec3efbd909589d81ae20d9f1cad1dc46d1833b
Yaroslav Muravskyi <y@myarik.com>
2021-01-31 11:10:43 +0100

    Finish the implementation of the alpha version

---
+++ consumer/consumer.py
@@ -0,0 +1,187 @@
+"""
+Module for reading data from Kafka and write them to the PostgreSQL
+"""
+from __future__ import annotations
+
+import asyncio
+import sys
+from typing import Set, Any, Optional, Callable
+
+import asyncpg
+from aiokafka import AIOKafkaConsumer
+from loguru import logger
+
+from consumer.serializer import KafkaDeserializer, REQUEST_SCHEMA
+from core.models import Response
+
+DEFAULT_POOL_SIZE = 3
+DEFAULT_DB_WORKERS = 3
+
+
+def _cancel_tasks(to_cancel: Set["asyncio.Task[Any]"], loop: asyncio.AbstractEventLoop):
+    if not to_cancel:
+        return
+
+    for task in to_cancel:
+        task.cancel()
+
+    loop.run_until_complete(
+        asyncio.gather(*to_cancel, loop=loop, return_exceptions=True)
+    )
+
+    for task in to_cancel:
+        if task.cancelled():
+            continue
+        if task.exception() is not None:
+            loop.call_exception_handler(
+                {
+                    "message": "unhandled exception during asyncio.run() shutdown",
+                    "exception": task.exception(),
+                    "task": task,
+                }
+            )
+
+
+async def kafka_consumer(
+    kafka_servers: str,
+    kafka_topic: str,
+    queue: asyncio.Queue[Response],
+    deserializer: Optional[Callable] = None,
+) -> None:
+    """
+    kafka_consumer reads data from kafka and send it to a queue
+    """
+    loop = asyncio.get_event_loop()
+
+    consumer = AIOKafkaConsumer(
+        kafka_topic,
+        loop=loop,
+        bootstrap_servers=kafka_servers,
+        group_id="my-group",
+        enable_auto_commit=True,
+        auto_commit_interval_ms=1000,  # Autocommit every second
+        auto_offset_reset="earliest",  # start from beginning
+        value_deserializer=deserializer,
+    )
+    await consumer.start()
+    try:
+        # Consume messages
+        async for msg in consumer:
+            if msg.value is not None:
+                logger.debug(f"Message received: {msg.value} at {msg.timestamp}")
+                try:
+                    asyncio.get_event_loop().call_soon_threadsafe(
+                        queue.put_nowait, msg.value
+                    )
+                except asyncio.QueueFull as err:
+                    logger.error("queue is full cannot send a response - {}", err)
+    finally:
+        # Will leave consumer group; perform autocommit if enabled.
+        await consumer.stop()
+
+
+async def run_worker(
+    worker_id: int, queue: asyncio.Queue[Response], pool: asyncpg.pool.Pool
+) -> None:
+    """
+    Send a response to kafka
+    """
+    insert_response = "INSERT INTO monitoring VALUES(DEFAULT, $1, $2, $3, $4, $5, $6)"
+    while True:
+        response = await queue.get()
+        # save a message
+        async with pool.acquire() as connection:
+            try:
+                await connection.execute(
+                    insert_response,
+                    response.url,
+                    response.load_time,
+                    response.status_code,
+                    response.ok,
+                    response.error,
+                    response.request_time,
+                )
+            except asyncpg.exceptions.PostgresConnectionError:
+                logger.error(f"[{worker_id}] cannot connect to postgresql")
+            except asyncpg.exceptions.DataError as err:
+                logger.error(f"[{worker_id}] invalid postgres query {err}")
+            except Exception as err:  # pylint: disable=broad-except
+                logger.error(f"[{worker_id}] unexpected error {err}")
+        queue.task_done()
+
+
+async def _run_app(
+    kafka_servers: str,
+    kafka_topic: str,
+    postgres_host: str,
+    postgres_port: int,
+    postgres_db: str,
+    postgres_user: str,
+    postgres_password: str,
+    deserializer: Optional[Callable] = None,
+) -> None:
+    queue: asyncio.Queue[Response] = asyncio.Queue(maxsize=512)
+    asyncio.create_task(kafka_consumer(kafka_servers, kafka_topic, queue, deserializer))
+    async with asyncpg.create_pool(
+        host=postgres_host,
+        port=postgres_port,
+        user=postgres_user,
+        password=postgres_password,
+        database=postgres_db,
+        min_size=DEFAULT_POOL_SIZE,
+        max_size=DEFAULT_POOL_SIZE,
+    ) as pool:
+        worker_tasks = [
+            asyncio.create_task(run_worker(index, queue, pool))
+            for index in range(DEFAULT_DB_WORKERS)
+        ]
+
+        await asyncio.gather(*worker_tasks)
+
+
+def run_app(
+    kafka_servers: str,
+    kafka_topic: str,
+    postgres_host: str,
+    postgres_port: int,
+    postgres_db: str,
+    postgres_user: str,
+    postgres_password: str,
+    *,
+    debug: bool = False,
+) -> None:
+    """run the consumer"""
+    loop = asyncio.get_event_loop()
+    loop.set_debug(debug)
+
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        colorize=True,
+        format="<green>{time}</green> <level>{level}</level>: {message}",
+        level="DEBUG" if debug else "INFO",
+    )
+    deserializer = KafkaDeserializer(REQUEST_SCHEMA)
+
+    try:
+        main_task = loop.create_task(
+            _run_app(
+                kafka_servers=kafka_servers,
+                kafka_topic=kafka_topic,
+                postgres_host=postgres_host,
+                postgres_port=postgres_port,
+                postgres_db=postgres_db,
+                postgres_user=postgres_user,
+                postgres_password=postgres_password,
+                deserializer=deserializer,
+            )
+        )
+        loop.run_forever()
+    except KeyboardInterrupt:  # pragma: no cover
+        pass
+    finally:
+        _cancel_tasks({main_task}, loop)
+        _cancel_tasks(asyncio.all_tasks(loop), loop)
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
+        asyncio.set_event_loop(None)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """See the docstring to main()."""
-import logging
 from pathlib import Path
 
 import click
 
 from monitoring.processor import run_app as run_monitoring
+from consumer.consumer import run_app as run_consumer
+from consumer.migrations.init import run as run_migration
 
 CURRENT_DIR = Path(__file__).parent
 
@@ -18,11 +19,30 @@ def main():
     Commands:
 
         monitoring   Start a monitoring service\n
-            --source-file filepath to the source file
-            --debug run application in the debug mode
+            --source-file filepath to the source file \n
+            --debug run application in the debug mode \n
+
+        consumer Service for storing monitoring data \n
+            --kafka_servers kafka bootstrap_servers \n
+            --kafka_topic kafka topic \n
+            --postgres_host PostgreSQL hostname \n
+            --postgres_port PostgreSQL port \n
+            --postgres_db PostgreSQL database \n
+            --postgres_user PostgreSQL user \n
+            --postgres_password PostgreSQL password \n
+            --debug run application in the debug mode \n
+
+        init-migration runs an init migration \n
+            --postgres_host PostgreSQL hostname \n
+            --postgres_port PostgreSQL port \n
+            --postgres_db PostgreSQL database \n
+            --postgres_user PostgreSQL user \n
+            --postgres_password PostgreSQL password \n
 
     Example:\n
         >>> ./main.py monitoring --debug \n
+        >>> ./main.py consumer --debug \n
+        >>> ./main.py init-migration\n
     """
 
 
@@ -41,7 +61,7 @@ def monitoring(
     source_file: str, kafka_servers: str, kafka_topic: str, debug: bool
 ) -> None:
     """
-    This is a module that implements the site checker and sends data to the Kafka.
+    Service checks sites and sends result to Kafka
 
     The input file is a JSON-format file that you may produce and modify with
     any convenient text editor. The file should have the following format:
@@ -57,7 +77,66 @@ def monitoring(
     )
 
 
+@click.command()
+@click.option("--kafka_servers", default="kafka:9093")
+@click.option("--kafka_topic", default="monitoring")
+@click.option("--postgres_host", default="postgres")
+@click.option("--postgres_port", default=5432)
+@click.option("--postgres_db", default="monitoring")
+@click.option("--postgres_user", default="demo")
+@click.option("--postgres_password", default="demopassword")
+@click.option("--debug", default=False, show_default=True, is_flag=True)
+def consumer(
+    kafka_servers: str,
+    kafka_topic: str,
+    postgres_host: str,
+    postgres_port: int,
+    postgres_db: str,
+    postgres_user: str,
+    postgres_password: str,
+    debug: bool,
+) -> None:
+    """
+    Service reads data from Kafka and writes to the database
+    """
+    click.echo("Starting consumer service ...")
+    run_consumer(
+        kafka_servers=kafka_servers,
+        kafka_topic=kafka_topic,
+        postgres_host=postgres_host,
+        postgres_port=postgres_port,
+        postgres_db=postgres_db,
+        postgres_user=postgres_user,
+        postgres_password=postgres_password,
+        debug=debug,
+    )
+
+
+@click.command()
+@click.option("--postgres_host", default="postgres")
+@click.option("--postgres_port", default=5432)
+@click.option("--postgres_db", default="monitoring")
+@click.option("--postgres_user", default="demo")
+@click.option("--postgres_password", default="demopassword")
+def init_migration(
+    postgres_host: str,
+    postgres_port: int,
+    postgres_db: str,
+    postgres_user: str,
+    postgres_password: str,
+) -> None:
+    """
+    runs init migration
+    """
+    click.echo("runs a migration ...")
+    run_migration(
+        postgres_host, postgres_port, postgres_db, postgres_user, postgres_password
+    )
+
+
 main.add_command(monitoring)
+main.add_command(consumer)
+main.add_command(init_migration)
 
 if __name__ == "__main__":
     main()
